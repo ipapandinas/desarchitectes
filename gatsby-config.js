39f7ec52f031ad9f89861c1acc3ce6eb8ccc66c3
Igor Papandinas <i.papandinas@gmail.com>
2020-06-02 22:29:40 +0200

    [TASK] Config manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: 'gatsby-source-strapi',
       options: {
         // apiURL: 'https://desarchitectes.herokuapp.com',
-        apiURL: process.env.API_URL || 'http://mgmt.desarchitectes.com',
+        apiURL: process.env.API_URL || 'http://localhost:1337',
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           'article',
@@ -37,13 +37,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'desarchitectes.com',
-        short_name: 'desarchitectes',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/d_favicon.png', // This path is relative to the root of the site.
+        name: `Abécédaire / Abecedario desarchitectes.com`,
+        short_name: `desarchitectes`,
+        start_url: `/`,
+        description: `Un abécédaire, un zoom sur des objets urbains. | Un abecedario, un zoom sobre objetos urbanos.`,
+        lang: `fr`,
+        background_color: `#fff`,
+        theme_color: `#000`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/d_favicon.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/d_favicon.png`,
+            sizes: `92x92`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     {
