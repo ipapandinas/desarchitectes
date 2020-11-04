7f14a806e029860aa66969c040efd9d3ac995eb8
Igor Papandinas <i.papandinas@gmail.com>
2020-11-04 18:49:43 +0100

    refactor(Letter): appContext used instead of redux

--- src/components/1-Atoms/Letter/Letter.jsx
+++ src/components/1-Atoms/Letter/Letter.jsx
@@ -1,38 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { useDispatch } from 'react-redux';
 
-import { useApp, usePageContext } from 'hooks';
-import { setLetter } from 'reduxApp';
+import { useAppContext, usePageContext } from 'hooks';
 
 import './Letter.scss';
 
-// indicator: '•', indicates the letters containing articles
-// selector: '>', selects the current letter
+function Letter(props) {
+  const { active, letter } = props;
 
-export default function Letter(props) {
-  const { letter } = props;
+  const { appData, setLetter } = useAppContext();
+  const { letters, preview, word } = appData;
 
-  const { letter: letterDisplayed, lettersUsed, preview, word } = useApp();
-  const dispatch = useDispatch();
   const { pageData } = usePageContext();
   const { lang } = pageData;
 
-  const { es, fr } = lettersUsed;
-  const hasArticle =
-    (lang === 'es' && es.some(l => l === letter)) ||
-    (lang === 'fr' && fr.some(l => l === letter));
-
-  let isSeparator = false;
-  if (preview) {
-    isSeparator = letter === letterDisplayed;
-  } else if (word) {
-    isSeparator = letter === word.charAt(0).toUpperCase();
-  }
+  const hasArticle = letters && letters.includes(letter);
+  const isIndicator =
+    (preview && active) ||
+    (!preview && word && letter === word.charAt(0).toUpperCase());
 
   return (
     <div
+      key={letter}
       className={classNames('Letter', {
         'Letter--ES': lang === 'es',
         'Letter--FR': lang === 'fr',
@@ -41,17 +31,13 @@ export default function Letter(props) {
       {hasArticle && (
         <button
           className={classNames('Letter__button', {
-            'Letter__button--active': isSeparator,
+            'Letter__button--active': isIndicator,
             'Letter__button--ES': lang === 'es',
             'Letter__button--FR': lang === 'fr',
           })}
           type="button"
-          onClick={() => {
-            dispatch(setLetter(letter));
-          }}
-          onMouseEnter={() => {
-            dispatch(setLetter(letter));
-          }}
+          onClick={() => setLetter(letter)}
+          onMouseEnter={() => setLetter(letter)}
         >
           <span>{letter}</span>
         </button>
@@ -60,14 +46,13 @@ export default function Letter(props) {
   );
 }
 
-Letter.defaultProps = {
-  lettersUsed: undefined,
+Letter.propTypes = {
+  active: PropTypes.bool,
+  letter: PropTypes.string.isRequired,
 };
 
-Letter.propTypes = {
-  letter: PropTypes.string.isRequired,
-  lettersUsed: PropTypes.shape({
-    es: PropTypes.arrayOf(PropTypes.string),
-    fr: PropTypes.arrayOf(PropTypes.string),
-  }),
+Letter.defaultProps = {
+  active: false,
 };
+
+export default Letter;
