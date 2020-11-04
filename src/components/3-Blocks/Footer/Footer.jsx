dc4206874efc370dedc6339cae44a38320ab4e62
Igor Papandinas <i.papandinas@gmail.com>
2020-11-04 19:02:29 +0100

    refactor(Footer): appContext used instead of redux / custom Link used

--- src/components/3-Blocks/Footer/Footer.jsx
+++ src/components/3-Blocks/Footer/Footer.jsx
@@ -1,37 +1,31 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'gatsby';
+import Link from 'components/5-Utils/Link/Link';
 
 import Image from 'components/1-Atoms/Image/Image';
 
-import { usePageContext } from 'hooks';
-import { reset } from 'reduxApp';
+import { useAppContext, usePageContext } from 'hooks';
 
 import './Footer.scss';
 
 export default function Footer() {
-  const dispatch = useDispatch();
+  const { setWord } = useAppContext();
   const { pageData } = usePageContext();
   const { lang } = pageData;
 
   return (
     <div className="Footer">
-      <button
-        className="Footer__button"
-        type="button"
+      <Link
+        className="Footer__link"
+        lang={lang}
+        onClick={() => setWord(undefined)}
         title={lang === 'es' ? 'Pagina de inició' : "Page d'accueil"}
-        onClick={() => {
-          dispatch(reset());
-        }}
       >
-        <Link className="Footer__link" title="Home" to="/">
-          <Image
-            className="Footer__logo"
-            alt="Logo desarchitectes"
-            filename="desarchitectes.png"
-          />
-        </Link>
-      </button>
+        <Image
+          className="Footer__logo"
+          alt="Logo desarchitectes"
+          filename="desarchitectes.png"
+        />
+      </Link>
     </div>
   );
 }
