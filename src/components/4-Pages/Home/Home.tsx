4b10da6270d28fe07c198d6dc4a177dca01eff93
Igor Papandinas <i.papandinas@gmail.com>
2020-11-16 19:46:27 +0100

    feat(Home): Credits added

--- src/components/4-Pages/Home/Home.tsx
+++ src/components/4-Pages/Home/Home.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
-import classNames from 'classnames';
+import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-import { usePageContext } from 'hooks';
+import TelmoIcon from 'assets/svg/telmo.svg';
 
-import './Home.scss';
+import layoutStyles from 'components/5-Utils/Layout/Layout.module.scss';
+
+import { usePageContext } from 'hooks';
+import formatNewLine from 'services/textFormat';
+
+import styles from './Home.module.scss';
 
 interface Props {
   content: string;
@@ -16,18 +21,19 @@ const Home: FC<Props> = ({ content }: Props) => {
   const { pageData } = usePageContext()!;
   const { lang } = pageData;
 
-  return (
-    <div className="Home fade-in">
-      <div
-        className={classNames(
-          'Home__content',
-          `Home__content--${lang?.toUpperCase()}`
-        )}
-      >
-        {content}
-      </div>
+  const creditsLabel = `© ${new Date().getFullYear()} desarchitectes`;
 
-      <div className="Home__socials">
+  const contentClass = `content${lang?.toLocaleUpperCase()}`;
+
+  return (
+    <div className={classnames(styles.root, 'fade-in')}>
+      {content && (
+        <div className={classnames(styles.content, contentClass)}>
+          {formatNewLine(content)}
+        </div>
+      )}
+
+      <div className={styles.socials}>
         <a
           href="https://www.instagram.com/desarchitectes/"
           title={
@@ -48,6 +54,23 @@ const Home: FC<Props> = ({ content }: Props) => {
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
       </div>
+
+      <footer className={classnames(styles.credits, layoutStyles.bold)}>
+        <span className={styles.ownCredits}>{creditsLabel}</span>
+        <a
+          className={styles.telmoCredits}
+          href="https://www.mrtelmo.com"
+          title="Mister Telmo Website"
+          rel="noopener noreferrer"
+        >
+          <TelmoIcon className={styles.telmoIcon} />
+          <div className={styles.telmoTag}>
+            <span className={styles.telmoTagMain}>mr. telmo</span>
+            <span className={styles.telmoTagSuffix}>code some more</span>
+            <span className={styles.line} />
+          </div>
+        </a>
+      </footer>
     </div>
   );
 };
